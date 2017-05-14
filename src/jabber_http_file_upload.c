e05bf8cf6bb06b49a69a40257b276f053ca230fc
Junker <dk-junker@ya.ru>
2017-05-14 21:51:15 +0700

    fix uploading large files (#3)

--- src/jabber_http_file_upload.c
+++ src/jabber_http_file_upload.c
@@ -292,9 +292,27 @@ static void jabber_hfu_xfer_cancel_send(PurpleXfer *xfer)
 
 static gssize jabber_hfu_xfer_write(const guchar *buffer, size_t len, PurpleXfer *xfer)
 {
+    gssize len;
+
     HFUXfer *hfux = purple_xfer_get_protocol_data(xfer);
 
-    return purple_ssl_write(hfux->ssl_conn, buffer, len);
+    len = purple_ssl_write(hfux->ssl_conn, buffer, len);
+
+    if (len == -1) 
+    {
+        if (purple_xfer_get_bytes_sent(xfer) >= purple_xfer_get_size(xfer))
+            purple_xfer_set_completed(xfer, TRUE);
+
+        if ((errno != EAGAIN) && (errno != EINTR))
+            return -1;
+        
+        return 0;
+    }
+
+    if ((purple_xfer_get_bytes_sent(xfer) + len) >= purple_xfer_get_size(xfer))
+            purple_xfer_set_completed(xfer, TRUE);
+
+    return len;
 }
 
 static void jabber_hfu_xfer_init(PurpleXfer *xfer)
